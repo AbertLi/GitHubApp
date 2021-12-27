2034f40d6b0af479b0a7abeb2e1ddfcbf623cfd0
Albertrt_Lee_BJB <3185017028@qq.com>
2021-12-27 23:43:46 +0800

    mvp框架搭建之BaseActivity实现。

---
+++ mvp/src/main/java/com/wifi/mvp/impl/BaseActivity.kt
@@ -0,0 +1,102 @@
+package com.wifi.mvp.impl
+
+import android.app.Activity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.wifi.mvp.IMvpView
+import com.wifi.mvp.IPresenter
+import java.lang.reflect.ParameterizedType
+import java.lang.reflect.Type
+import kotlin.reflect.KClass
+import kotlin.reflect.full.isSubclassOf
+import kotlin.reflect.full.primaryConstructor
+import kotlin.reflect.jvm.jvmErasure
+
+
+abstract class BaseActivity<out P : BasePresenter<BaseActivity<P>>> : IMvpView<P>, AppCompatActivity(){
+    override val presenter: P
+    init {
+        presenter = createPresenterKt()
+        presenter.view = this
+    }
+
+    private fun createPresenterKt(): P {//使用反射获取泛型
+        sequence {
+            var thisClass: KClass<*> = this@BaseActivity::class
+            while (true) {
+                yield(thisClass.supertypes)
+                thisClass = thisClass.supertypes.firstOrNull()?.jvmErasure ?: break
+            }
+        }.flatMap {
+            it.flatMap { it.arguments }.asSequence()
+        }.first {
+            it.type?.jvmErasure?.isSubclassOf(IPresenter::class) ?: false
+        }.let {
+            return it.type!!.jvmErasure.primaryConstructor!!.call() as P
+        }
+    }
+
+    private fun createPresenter(): P {
+        sequence<Type> {
+            var thisClass: Class<*> = this@BaseActivity.javaClass
+            while (true) {
+                yield(thisClass.genericSuperclass)
+                thisClass = thisClass.superclass ?: break
+            }
+        }.filter {
+            it is ParameterizedType
+        }.flatMap {
+            (it as ParameterizedType).actualTypeArguments.asSequence()
+        }.first {
+            it is Class<*> && IPresenter::class.java.isAssignableFrom(it)
+        }.let {
+            return (it as Class<P>).newInstance()
+        }
+    }
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        presenter.onCreate(savedInstanceState)
+    }
+
+    override fun onViewStateRestored(saveInstanceState: Bundle?) {
+    }
+
+    override fun onStart() {
+        super.onStart()
+        presenter.onStart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        presenter.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        presenter.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        presenter.onStop()
+    }
+
+    override fun onDestroy() {
+        presenter.onDestroy()
+        super.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        presenter.onSaveInstanceState(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        onViewStateRestored(savedInstanceState)
+        presenter.onViewStateRestored(savedInstanceState)
+    }
+}
